c2ac1cc33809eb975528ab910c079be27fd40046
Vinícius Côrte <cortevinicius1@gmail.com>
2026-08-14 12:39:24 -0300

    corrige github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      margin: 0,
+      height: '100vh',
+      background: '#111827',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+      color: 'white',
+      fontFamily: 'Arial'
+    }}>
+
+      <h1>🐍 Jogo da Cobrinha</h1>
+
+      <p>Seu jogo está online no GitHub Pages! 🎮</p>
+
+      <div style={{
+        width: 300,
+        height: 300,
+        background: '#000',
+        border: '4px solid #22c55e',
+        borderRadius: 12,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 60
+      }}>
+        🐍
+      </div>
+
     </div>
   );
 }
-
-export default App;
